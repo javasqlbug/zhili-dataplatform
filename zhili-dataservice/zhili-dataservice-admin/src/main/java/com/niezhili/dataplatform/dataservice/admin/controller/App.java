22ab55b747c0000b2de90c609d547885c7127ea6
聂知理 <niezhili@192.168.0.17>
2021-05-23 09:52:14 +0800

    Initial commit

---
+++ zhili-dataservice/zhili-dataservice-admin/src/main/java/com/niezhili/dataplatform/dataservice/admin/controller/App.java
@@ -0,0 +1,13 @@
+package com.niezhili.dataplatform.dataservice.admin.controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ zhili-dataservice/zhili-dataservice-api/src/main/java/com/niezhili/dataplatform/dataservice/App.java
@@ -0,0 +1,13 @@
+package com.niezhili.dataplatform.dataservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
